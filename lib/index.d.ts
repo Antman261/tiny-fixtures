4b4be99d84216647b5eb2824ef46500c709097e3
Anthony Manning-Franklin <anthony.manning.franklin@gmail.com>
2021-05-13 20:31:52 +0800

    Prepare for publish

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,12 @@
-export declare const sum: (a: number, b: number) => number;
+import { Pool } from "pg";
+declare type SetupFixtures = () => void;
+declare type TeardownFixtures = () => void;
+declare type RowHelpers = {
+    getRefByKey: (key: string) => () => string | number;
+};
+declare type TinyFixtures = {
+    createFixtures: <T extends object>(table: string, rows: T[], primaryKeyName?: string) => [SetupFixtures, TeardownFixtures, Array<T & RowHelpers>];
+};
+export declare const tinyFixtures: (pool: Pool) => TinyFixtures;
+export {};
 //# sourceMappingURL=index.d.ts.map
