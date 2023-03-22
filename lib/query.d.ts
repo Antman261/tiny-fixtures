53161304742e50d00699f18fe36210a314bec1bd
真壁輝 <akira.makabe@systemi.co.jp>
2023-03-22 15:11:56 +0900

    feat: allow pg-camelcase'd identifiers
    add camelCased flag to use pg-camelcase

--- lib/query.d.ts
+++ lib/query.d.ts
@@ -1,7 +1,7 @@
 import { Pool, QueryResult } from 'pg';
 declare type Row = Object;
-export declare const buildDeleteQueryString: (table: string, pkName: string, keys: Array<string | number>) => string;
+export declare const buildDeleteQueryString: (table: string, pkName: string, keys: Array<string | number>, camelCased: boolean) => string;
 export declare const findPrimaryKeyName: ({ fields }: Pick<QueryResult, 'fields'>) => string;
-export declare const createRowToQueryMapper: (table: string, pool: Pool) => (row: Row) => Promise<QueryResult>;
+export declare const createRowToQueryMapper: (table: string, pool: Pool, camelCased: boolean) => (row: Row) => Promise<QueryResult>;
 export {};
 //# sourceMappingURL=query.d.ts.map
