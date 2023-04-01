ad9b1785436be9eaca2af0b695069f1ecee04e8c
Seth Phat <phattranminh96@gmail.com>
2023-04-01 13:23:53 +0700

    Improve the `SetupFixtures` return type (#6)
    * improve the setup type
    
    * use generic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'pg';
+import { Pool, QueryResult } from 'pg';
 import {
   buildDeleteQueryString,
   createRowToQueryMapper,
@@ -8,7 +8,7 @@ import {
 /**
  * Call this function inside your `before` or `beforeEach` step to insert the specified fixtures
  */
-export type SetupFixtures = () => void;
+export type SetupFixtures<T> = () => Promise<QueryResult<T>[]>;
 
 /**
  * Call this function inside your `after` or `afterEach` step to delete the specified fixtures. This will only delete the data inserted for this fixture, so any other test data remains untouched.
@@ -55,7 +55,7 @@ export type CreateFixtures = <T extends object>(
   table: string,
   rows: T[],
   primaryKeyName?: string
-) => [SetupFixtures, TeardownFixtures, ResultArray<T>];
+) => [SetupFixtures<T>, TeardownFixtures, ResultArray<T>];
 
 /**
  * Contains the createFixtures function, with the pool in its closure.
